d166a585319d254731c987b9ef00057126b35a02
Tomer Klein <tomer.klein@gmail.com>
2020-09-07 23:00:11 +0300

    Update dockerbot.py

--- dockerbot.py
+++ dockerbot.py
@@ -7,14 +7,12 @@ from subprocess import call
 import subprocess
 import os
 import sys
+import docker
 from telepot.loop import MessageLoop
 
 #Vars for Selenium covid kids approval
 v_UserId = os.getenv('USER_ID')
 v_UserKey = os.getenv('USER_KEY') 
-v_KidTotal = os.getenv('KIDS_NUM')
-#Auto Commmand List
-
 
 def handle(msg):
     chat_id = msg['chat']['id']
@@ -28,14 +26,17 @@ def handle(msg):
     if command == '/sign':
         v_Kid = "sign"
         try:
-            subprocess.check_output(['python', '/etc/Health_Staytments.py', '-u', v_UserId, '-p', v_UserKey, '-k', v_Kid, '-n', str(v_KidTotal)])
+            subprocess.check_output(['python', '/etc/Health_Staytments.py', '-u', v_UserId, '-p', v_UserKey, '-k', v_Kid])
             for file in os.listdir("/opt"):
                 if file.endswith(".png"):
                     Image = os.path.join("/opt", file)
             bot.sendPhoto(chat_id=chat_id, photo=open(str(Image), 'rb'))
+            os.remove(str(Image))
         except:
             x = "Error"
             bot.sendMessage(chat_id,x)
+
+
 bot = telepot.Bot(os.getenv('API_KEY'))
 MessageLoop(bot, handle).run_as_thread()
 print('I am listening ...')
