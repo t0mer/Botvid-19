93e012e67791e28a0452131345347566173be62a
Yonatan Bitton <yobitton@microsoft.com>
2020-09-10 01:48:58 +0300

    add logs and ignore certificates errors in browser and more

--- Health_Staytments.py
+++ Health_Staytments.py
@@ -8,6 +8,8 @@ import time
 from argparse import ArgumentParser
 import os
 from selenium.common.exceptions import InvalidSessionIdException
+from loguru import logger
+
 
 parser = ArgumentParser()
 parser.add_argument("-u", "--user", dest="userCode")
@@ -27,25 +29,33 @@ option.add_argument("--no-sandbox")
 option.add_argument('--start-maximized')
 option.add_argument("--disable-dev-shm-usage")
 option.add_argument("--window-size=800,600")
+option.add_argument('--ignore-certificate-errors')
 
 
 def fullpage_screenshot():
-    print(browser)
+    logger.info(browser)
     browser.set_window_size(800, 600) #the trick
     time.sleep(2)
     image = "/opt/Approval_form.png"
     browser.save_screenshot(image)
     browser.close()
 
+def log_browser(browser):
+    logger.debug(f"Opened page. Url: {browser.current_url}, size: {len(browser.page_source)}")
 
-print("Strating process")
+
+logger.info("Starting process")
 
 browser = webdriver.Chrome(executable_path="/opt/chromedriver-85.0.4183.87/chromedriver", options=option)
 browser.get("https://parents.education.gov.il/prhnet/parents/rights-obligations-regulations/health-statement-kindergarden")
 start = '//*[@id="main-content"]/section[1]/div/health-declaration/div/div[1]/div[4]/div/div/div/input'
-time.sleep( 2 )
+time.sleep(2)
+log_browser(browser)
+
 browser.find_element_by_xpath(start).click()
-time.sleep( 2 )
+time.sleep(2)
+log_browser(browser)
+
 user = '//*[@id="HIN_USERID"]'
 siteAccess = '//*[@id="Ecom_Password"]'
 NextPhase = '//*[@id="loginButton2"]'
@@ -53,25 +63,31 @@ NextPhase = '//*[@id="loginButton2"]'
 browser.find_element_by_xpath(user).send_keys(userCode)
 browser.find_element_by_xpath(siteAccess).send_keys(SitePassword)
 browser.find_element_by_xpath(NextPhase).click()
-time.sleep( 2 )
-print("Logged IN...")
+time.sleep(2)
+log_browser(browser)
+
+logger.info(f"Logged IN... url: {browser.current_url}")
+
 element = "//input[@value='מילוי הצהרת בריאות']"
+
 checkForButton = browser.find_elements_by_xpath(element)
+
 LenCheckForButton = len(checkForButton)
+
 if KidCovid == 'sign':
-    print("starting Sign..")
-    print(len(checkForButton))
-    if int(LenCheckForButton) == 0:
+    logger.info("starting Sign..")
+    logger.info(LenCheckForButton)
+    if LenCheckForButton == 0:
         fullpage_screenshot()
     elif LenCheckForButton > 0:
         for x in range(LenCheckForButton):
-            print(x + 1) 
+            logger.info(x + 1) 
             browser.find_element_by_xpath(element).click()
-            time.sleep( 2 )
+            time.sleep(2)
             ToApprove = browser.find_elements_by_xpath("//input[@value='אישור']")
             for a in ToApprove:
                 browser.execute_script("arguments[0].click()", a)
-            time.sleep( 2 )
+            time.sleep(2)
         fullpage_screenshot()
     else:
-        print("Used else...")
+        logger.info("Used else...")
--- dockerbot.py
+++ dockerbot.py
@@ -1,3 +1,5 @@
+import sys
+from loguru import logger
 import time, re, random, datetime, telepot
 from subprocess import call
 import subprocess, os, sys
@@ -8,14 +10,21 @@ v_UserId = os.getenv('USER_ID')
 v_UserKey = os.getenv('USER_KEY') 
 
 def handle(msg):
+    message_id = msg['message_id'] 
+    msg_logger = logger.bind(message_id=message_id)
+
+    msg_logger.info(f"Got msg: {msg}")
+
     chat_id = msg['chat']['id']
     command = msg['text']
     
     if str(chat_id) not in os.getenv('ALLOWED_IDS'):
-        bot.sendPhoto(chat_id,"https://github.com/t0mer/dockerbot/raw/master/No-Trespassing.gif")
+        bot.sendPhoto(chat_id, "https://github.com/t0mer/dockerbot/raw/master/No-Trespassing.gif")
+        msg_logger.info(f"Chat id not allowed: {chat_id}")
         return ""
 
-    print ('Got command: %s')%command
+    msg_logger.info(f"Got command: {command}")
+
     if command == '/sign':
         v_Kid = "sign"
         try:
@@ -25,14 +34,21 @@ def handle(msg):
                     Image = os.path.join("/opt", file)
             bot.sendPhoto(chat_id=chat_id, photo=open(str(Image), 'rb'))
             os.remove(str(Image))
-        except:
-            x = "Error"
-            bot.sendMessage(chat_id,x)
+            msg_logger.info(f"Return result to command {command}. Result image path: {Image}")
+        except Exception as ex:
+            msg_logger.exception(f"Failed to handle command. Msg: {msg}")
+            bot.sendMessage(chat_id, f"ERROR: {str(ex)}")
+
+    msg = f"Done message handling: {command}"
+    bot.sendMessage(chat_id, msg)
+    msg_logger.info(msg)
 
 
 bot = telepot.Bot(os.getenv('API_KEY'))
+
 MessageLoop(bot, handle).run_as_thread()
-print('I am listening ...')
+
+logger.info('I am listening...')
  
 while 1:
     time.sleep(10)
