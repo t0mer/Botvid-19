ea7fe671dc4d244ce310e14dcace2a777d25f07f
Yonatan Bitton <yobitton@microsoft.com>
2020-09-11 01:43:01 +0300

    Refactor Health_Statements.py

--- Health_Statements.py
+++ Health_Statements.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from datetime import date
 from datetime import time
 from datetime import datetime
 import time
 from argparse import ArgumentParser
 import os
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import InvalidSessionIdException
 from loguru import logger
 
@@ -35,67 +36,94 @@ option.add_argument("--window-size=800,600")
 option.add_argument('--ignore-certificate-errors')
 
 
-def fullpage_screenshot():
-    logger.info(f"[{message_id}] Saving screenshot from browser: {browser}")
-    browser.set_window_size(800, 600) #the trick
+def full_page_screenshot(browser):
+    image_file = f"/opt/Approval_form_{message_id}.png"
+
+    logger.info(f"[{message_id}] Saving screenshot from browser: {browser}, image_file: {image_file}")
+
+    browser.set_window_size(800, 600)  # the trick
     time.sleep(2)
-    image = f"/opt/Approval_form_{message_id}.png"
-    browser.save_screenshot(image)
+
+    browser.save_screenshot(image_file)
     browser.close()
-    return image
+
 
 def log_browser(browser):
     logger.debug(f"[{message_id}] Opened page. Url: {browser.current_url}, size: {len(browser.page_source)}")
 
 
-logger.info(f"[{message_id}] Starting process")
+def login(browser):
+    user = '//*[@id="HIN_USERID"]'
+    site_access = '//*[@id="Ecom_Password"]'
+    next_phase = '//*[@id="loginButton2"]'
 
-browser = webdriver.Chrome(executable_path="/opt/chromedriver-85.0.4183.87/chromedriver", options=option)
-browser.get("https://parents.education.gov.il/prhnet/parents/rights-obligations-regulations/health-statement-kindergarden")
-start = '//*[@id="main-content"]/section[1]/div/health-declaration/div/div[1]/div[4]/div/div/div/input'
-time.sleep(2)
-log_browser(browser)
+    browser.find_element_by_xpath(user).send_keys(user_id)
+    browser.find_element_by_xpath(site_access).send_keys(user_password)
+    browser.find_element_by_xpath(next_phase).click()
+    time.sleep(2)
+    log_browser(browser)
 
-browser.find_element_by_xpath(start).click()
-time.sleep(2)
-log_browser(browser)
 
-user = '//*[@id="HIN_USERID"]'
-site_access = '//*[@id="Ecom_Password"]'
-next_phase = '//*[@id="loginButton2"]'
+def sign_parents_portal(browser):
+    # 1
+    browser.get(
+        "https://parents.education.gov.il/prhnet/parents/rights-obligations-regulations/health-statement-kindergarden")
+    start = '//*[@id="main-content"]/section[1]/div/health-declaration/div/div[1]/div[4]/div/div/div/input'
+    time.sleep(2)
+    log_browser(browser)
 
-browser.find_element_by_xpath(user).send_keys(user_id)
-browser.find_element_by_xpath(site_access).send_keys(user_password)
-browser.find_element_by_xpath(next_phase).click()
-time.sleep(2)
-log_browser(browser)
+    # 2
+    browser.find_element_by_xpath(start).click()
+    time.sleep(2)
+    log_browser(browser)
 
-logger.info(f"[{message_id}] Logged in")
+    # 3
+    browser.find_element_by_xpath(start).click()
+    time.sleep(2)
+    log_browser(browser)
 
-element = "//input[@value='מילוי הצהרת בריאות']"
+    # 4
+    login(browser)
+    logger.info(f"[{message_id}] Logged in")
 
-checkForButton = browser.find_elements_by_xpath(element)
+    # 5
+    element = "//input[@value='מילוי הצהרת בריאות']"
+    kids_checks_buttons = browser.find_elements_by_xpath(element)
 
-LenCheckForButton = len(checkForButton)
+    # 6
+    len_kids_checks_buttons = len(kids_checks_buttons)
+    logger.info(f"[{message_id}] Starting sign... check buttons: {len_kids_checks_buttons}")
 
-if KidCovid == 'sign':
-    logger.info(f"[{message_id}] Starting sign... check buttons: {LenCheckForButton}")
-
-    if LenCheckForButton == 0:
+    if len_kids_checks_buttons <= 0:
         logger.error(f"[{message_id}] Not able to find the check buttons. Exit")
-        fullpage_screenshot()
+        full_page_screenshot(browser)
+        return
 
-    elif LenCheckForButton > 0:
-        for x in range(LenCheckForButton):
-            logger.info(x + 1) 
-            browser.find_element_by_xpath(element).click()
-            time.sleep(2)
-            ToApprove = browser.find_elements_by_xpath("//input[@value='אישור']")
-            
-            for a in ToApprove:
-                browser.execute_script("arguments[0].click()", a)
-            time.sleep(2)
+    # 7
+    for x in range(len_kids_checks_buttons):
+        logger.info(x + 1)
+        browser.find_element_by_xpath(element).click()
+        time.sleep(2)
 
-        fullpage_screenshot()
-    else:
-        logger.error(r"[{message_id}] Used else...")
+        ToApprove = browser.find_elements_by_xpath("//input[@value='אישור']")
+
+        for a in ToApprove:
+            browser.execute_script("arguments[0].click()", a)
+        time.sleep(2)
+
+    full_page_screenshot(browser)
+
+
+def main():
+    logger.info(f"[{message_id}] Starting process")
+
+    if KidCovid != 'sign':
+        return
+
+    browser = webdriver.Chrome(executable_path="/opt/chromedriver-85.0.4183.87/chromedriver", options=option)
+
+    sign_parents_portal(browser)
+
+
+if __name__ == '__main__':
+    main()
