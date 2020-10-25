216822c9ce60da6b3e07631cd494bb5682f9693c
T0mer <tomer.klein@gmail.com>
2020-10-26 00:08:24 +0200

    Fix Mashov

--- dockerbot.py
+++ dockerbot.py
@@ -1,26 +1,24 @@
 from loguru import logger
-import time
-import re
-import random
-import datetime
-import telepot
+import time, re, random, datetime, telepot, os, sys
 from subprocess import call
-import subprocess
-import os
-import sys
+import subprocess, yaml, shutil
 from os import path
 from telepot.loop import MessageLoop
 from dotenv import load_dotenv
-import yaml
-import shutil
 
 configfile="/opt/dockerbot/config/config.yml"
-if not os.path.exists(configfile):
-    shutil.copyfile('/etc/config.yml', configfile)
+original_configfile = r'/etc/config.yml'
 
 
-# get YAML with Configs
-with open(configfile, 'r') as stream:
+def copyConfig():
+    if not os.path.exists(configfile):
+        shutil.copyfile(original_configfile, configfile)
+
+
+copyConfig()
+
+# Load Configuration
+with open("/opt/dockerbot/config/config.yml", 'r') as stream:
     try:
         list = yaml.safe_load(stream)
     except yaml.YAMLError as exc:
@@ -34,10 +32,10 @@ if v_MASHOV_NUMBER_OF_KIDS >= 1 and list['mashov']['kid1']['MASHOV_USER_ID_KID']
 def handle(msg):
     config_edu = 1
     config_mashov = 1
-    message_id = msg['message_id']
+    message_id = str(msg['message_id'])
     chat_id = msg['chat']['id']
-    command = msg['text']
-    logger.info("Got msg: " + command)
+    _command = command = str(msg['text'])
+    logger.info("{message_id} Got msg: {command}")
 
     # Reject unauthorized requests
     if str(chat_id) not in os.getenv('ALLOWED_IDS'):
@@ -60,8 +58,6 @@ def handle(msg):
         if list['webtop']['USER_ID'] and list['webtop']['USER_KEY'] != None:
             bot.sendMessage(
                 chat_id, "/sign_webtop - This command start the sign process at https://www.webtop.co.il/v2/? ")
-        bot.sendMessage(
-            chat_id, "/sign_all - This command starts *All* Configured signing process ")
 
     if command == '/sign_edu' or command == '/sign':
         if list['edu']['USER_ID'] and list['edu']['USER_KEY'] != None:
@@ -113,6 +109,8 @@ def handle(msg):
         if list['webtop']['USER_ID'] and list['webtop']['USER_KEY'] != None:
             try:
                 Image = '/opt/dockerbot/images/webtop_approval.png'
+                bot.sendMessage(
+                    chat_id, "Starting Sign process at https://www.webtop.co.il/mobilev2/?")
                 import Webtop_Health_Statements
                 if Webtop_Health_Statements.sign(list['webtop']['USER_ID'], list['webtop']['USER_KEY'], Image) == 1:
                     time.sleep(2)
@@ -127,8 +125,10 @@ def handle(msg):
         else:
             bot.sendMessage(chat_id, "webtop NOT configured")
     if command == '/sign_mashov':
+        Image = '/opt/dockerbot/images/mashov_approval_'
         if v_MASHOV_NUMBER_OF_KIDS >= 1 and list['mashov']['kid1']['MASHOV_USER_ID_KID'] != None:
             try:
+                import Mashov_Health_Statements
                 if v_MASHOV_NUMBER_OF_KIDS >= 1 and list['mashov']['kid1']['MASHOV_USER_ID_KID'] != None:
                     for Mashov_Kid_Number in range(1, v_MASHOV_NUMBER_OF_KIDS, 1):
                         if list['mashov']['kid'+str(Mashov_Kid_Number)]['MASHOV_USER_ID_KID'] and list['mashov']['kid'+str(Mashov_Kid_Number)]['MASHOV_USER_PWD_KID'] and list['mashov']['kid'+str(Mashov_Kid_Number)]['MASHOV_SCHOOL_ID_KID'] != None:
@@ -136,14 +136,14 @@ def handle(msg):
                             Prep_Switch_MASHOV_USER_DICT_ID_KID = list['mashov']['kid'+str(Mashov_Kid_Number)]['MASHOV_USER_ID_KID']
                             Prep_Switch_MASHOV_USER_DICT_ID_PWD = list['mashov']['kid'+str(Mashov_Kid_Number)]['MASHOV_USER_PWD_KID']
                             Prep_Switch_MASHOV_USER_DICT_ID_SCHOOL_ID = list['mashov']['kid'+str(Mashov_Kid_Number)]['MASHOV_SCHOOL_ID_KID']                     
-                            subprocess.check_output(['python', '/opt/dockerbot/Mashov_Health_Statements.py', '-u', Prep_Switch_MASHOV_USER_DICT_ID_KID, '-p', Prep_Switch_MASHOV_USER_DICT_ID_PWD , '-s', Prep_Switch_MASHOV_USER_DICT_ID_SCHOOL_ID, '-kn', str(Mashov_Kid_Number)])
+                            Mashov_Health_Statements.sign(Prep_Switch_MASHOV_USER_DICT_ID_KID, Prep_Switch_MASHOV_USER_DICT_ID_PWD, Prep_Switch_MASHOV_USER_DICT_ID_SCHOOL_ID, str(Mashov_Kid_Number), Image + str(Mashov_Kid_Number) + ".png")
                 else:
                     bot.sendMessage(chat_id, "mashov NOT configured")
                     config_mashov = 0
 
-                for file in os.listdir("/opt"):
-                    if file.endswith(".png") and file.startswith("mashovkid"):
-                        Image = os.path.join("/opt", file)
+                for file in os.listdir("/opt/dockerbot/images"):
+                    if file.endswith(".png") and file.startswith("mashov"):
+                        Image = os.path.join("/opt/dockerbot/images", file)
                         bot.sendPhoto(chat_id=chat_id, photo=open(str(Image), 'rb'))
                         os.remove(str(Image))
                         logger.info(f"[{message_id}] Return result to command {command}. Result image path: {Image}")
@@ -157,104 +157,15 @@ def handle(msg):
     msg = f"Done message handling: {command}"
     logger.info(f"[{message_id}] {msg}")
 
-
-    if command == '/sign_all':
-        if list['edu']['USER_ID'] and list['edu']['USER_KEY'] != None:
-            Image = '/opt/dockerbot/images/edu_approval.png'
-            try:
-                if list['edu']['USER_ID'] != None:
-                    bot.sendMessage(
-                        chat_id, "Starting Sign process at https://parents.education.gov.il")
-                    import Health_Statements
-                    if Health_Statements.sign(str(list['edu']['USER_ID']), list['edu']['USER_KEY'], Image) == 1:
-                        time.sleep(1)
-                        bot.sendPhoto(chat_id=chat_id,
-                                      photo=open(str(Image), 'rb'))
-                        bot.sendMessage(chat_id, "Signed")
-                        logger.info(
-                            f"[{message_id}] Return result to command {command}. Result image path: {Image}")
-                    else:
-                        bot.sendMessage(
-                            chat_id, "Well, Somthing went wrong, please check the logs for more info")
-                else:
-                    bot.sendMessage(chat_id, "edu NOT configured")
-                    config_edu = 0
-            except Exception as ex:
-                logger.exception(
-                    f"[{message_id}] Failed to handle command. Msg: {command}")
-                bot.sendMessage(chat_id, f"ERROR: {str(ex)}")
-        if v_MASHOV_NUMBER_OF_KIDS >= 1 and list['mashov']['kid1']['MASHOV_USER_ID_KID'] != None:
-             try:
-                 if v_MASHOV_NUMBER_OF_KIDS >= 1 and list['mashov']['kid1']['MASHOV_USER_ID_KID'] != None:
-                     for Mashov_Kid_Number in range(1, v_MASHOV_NUMBER_OF_KIDS, 1):
-                         bot.sendMessage(chat_id,"Starting Sign process at https://web.mashov.info/students/login for Kid Number: " + str(Mashov_Kid_Number))
-                         Prep_Switch_MASHOV_USER_DICT_ID_KID = list['mashov']['kid'+str(Mashov_Kid_Number)]['MASHOV_USER_ID_KID']
-                         Prep_Switch_MASHOV_USER_DICT_ID_PWD = list['mashov']['kid'+str(Mashov_Kid_Number)]['MASHOV_USER_PWD_KID']
-                         Prep_Switch_MASHOV_USER_DICT_ID_SCHOOL_ID = list['mashov']['kid'+str(Mashov_Kid_Number)]['MASHOV_SCHOOL_ID_KID']                     
-                         subprocess.check_output(['python', '/opt/dockerbot/Mashov_Health_Statements.py', '-u', Prep_Switch_MASHOV_USER_DICT_ID_KID, '-p', Prep_Switch_MASHOV_USER_DICT_ID_PWD , '-s', Prep_Switch_MASHOV_USER_DICT_ID_SCHOOL_ID, '-kn', str(Mashov_Kid_Number)])
-                 else:
-                     bot.sendMessage(chat_id, "mashov NOT configured")
-                     config_mashov = 0
-
-                 for file in os.listdir("/opt"):
-                     if file.endswith(".png") and file.startswith("mashovkid"):
-                         Image = os.path.join("/opt", file)
-                         bot.sendPhoto(chat_id=chat_id, photo=open(str(Image), 'rb'))
-                         os.remove(str(Image))
-                         logger.info(f"[{message_id}] Return result to command {command}. Result image path: {Image}")
-                 if config_mashov != 0:
-                     bot.sendMessage(chat_id, "Signed")
-             except Exception as ex:
-                 logger.exception(f"[{message_id}] Failed to handle command. Msg: {command}")
-                 bot.sendMessage(chat_id, f"ERROR: {str(ex)}")
-        if list['infogan']['BASE_URL'] and list['infogan']['KID_ID'] and list['infogan']['PARENT_NAME'] and list['infogan']['KID_NAME'] and list['infogan']['PARENT_ID']  != None:
-            Image = '/opt/dockerbot/images/infogan_approval.png'
-            try:
-                bot.sendMessage(
-                    chat_id, "Starting Sign process at https://https://campaign.infogan.co.il/")
-                import Infogan_Health_Statements
-                if Infogan_Health_Statements.sign(list['infogan']['PARENT_NAME'], str(list['infogan']['PARENT_ID']),  list['infogan']['KID_NAME'], str(list['infogan']['KID_ID']), list['infogan']['BASE_URL'], Image) == 1:
-                    bot.sendPhoto(chat_id=chat_id, photo=open(str(Image), 'rb'))
-                    time.sleep(1)
-                    os.remove(str(Image))
-                    logger.info(f"[{message_id}] Return result to command {command}. Result image path: {Image}")
-                    bot.sendMessage(chat_id, "Signed")
-                else:
-                    bot.sendMessage(chat_id, "Well, Somthing went wrong, please check the logs for more info")
-            except Exception as ex:
-                logger.exception(
-                    f"[{message_id}] Failed to handle command. Msg: {command}")
-                bot.sendMessage(chat_id, f"ERROR: {str(ex)}")
-        if list['webtop']['USER_ID'] and list['webtop']['USER_KEY'] != None:
-            try:
-                Image = '/opt/dockerbot/images/webtop_approval.png'
-                import Webtop_Health_Statements
-                if Webtop_Health_Statements.sign(list['webtop']['USER_ID'], list['webtop']['USER_KEY'], Image) == 1:
-                    time.sleep(2)
-                    bot.sendPhoto(chat_id=chat_id, photo=open(str(Image), 'rb'))
-                    logger.info(f"[{message_id}] Return result to command {command}. Result image path: {Image}")
-                    bot.sendMessage(chat_id, "Signed")
-                else:
-                    bot.sendMessage(chat_id, "Well, Somthing went wrong, please check the logs for more info")
-            except Exception as ex:
-                logger.exception(f"[{message_id}] Failed to handle command. Msg: {command}")
-                bot.sendMessage(chat_id, f"ERROR: {str(ex)}")
-
-
 bot = telepot.Bot(os.getenv('API_KEY'))
-
 MessageLoop(bot, handle).run_as_thread()
-
 logger.info('I am listening...')
+
 
 while 1:
     time.sleep(10)
-    #check Conifg is existed
-    original = r'/etc/config.yml'
-    target = r'/opt/dockerbot/config/config.yml'
-
-    if os.path.isfile(target):
+    if os.path.isfile(configfile):
         continue
     else:
-        shutil.copyfile(original, target)
+        copyConfig()
         logger.error("Recoverd Config to /opt/dockerbot/config/")
--- workers/Mashov_Health_Statements.py
+++ workers/Mashov_Health_Statements.py
@@ -1,99 +1,85 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-import time
-import os.path
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from argparse import ArgumentParser
+from datetime import date, time, datetime
 from selenium.common.exceptions import InvalidSessionIdException
+import os, time, helpers
 from loguru import logger
 
 
-driver = webdriver.Remote("http://127.0.0.1:4444/wd/hub", DesiredCapabilities.CHROME)
-parser = ArgumentParser()
-parser.add_argument("-u", "--user", dest="Mashov_Username")
-parser.add_argument("-p", "--pass", dest="Mashov_Password")
-parser.add_argument("-s", "--schoolid", dest="Mashov_School_ID")
-parser.add_argument("-kn", "--kidnum", dest="Mashov_Kid_Number")
+def sign(user, password, schoolid, kidnum, Image):
+    try:
+        browser = helpers.GetBrowser()
+        logger.info("Starting process")
 
-args = parser.parse_args()
-var_screenshot_path = "/opt"
-var_mashov_username = args.Mashov_Username
-var_mashov_password = args.Mashov_Password
-var_mashov_school_id = args.Mashov_School_ID
-var_mashov_kid_number = args.Mashov_Kid_Number
-
-option = webdriver.ChromeOptions()
-option.add_argument("--window-size=800,600")
-option.add_argument("disable-gpu")
-option.add_argument("--no-sandbox")
-option.add_argument("--disable-dev-shm-usage")
-option.add_argument('--start-maximized')
-option.add_argument("-incognito")
-option.add_argument("--headless")
-option.add_experimental_option("excludeSwitches", ['enable-automation'])
-
-logger.debug('----------------------------------------------------------------------')
-logger.info("Starting Mashov Sign process for kid number: " + var_mashov_kid_number)
+        try:
+            helpers.ping(browser, 'infogan')
+        except:
+            logger.debug('Unable to ping')
+        logger.debug('----------------------------------------------------------------------')
+        logger.info("Starting Mashov Sign process for kid number: " + kidnum)
+        
+        browser.get("https://web.mashov.info/students/login")
+        time.sleep(3)
+              
+        #choose school:
+        form_element_mashov_select_school = browser.find_element_by_xpath("//*[@id='mat-input-3']")
+        form_element_mashov_user = browser.find_element_by_xpath("//*[@id='mat-input-0']")
+        form_element_mashov_password = browser.find_element_by_xpath("//*[@id='mat-input-4']")
+        form_element_mashov_login = browser.find_element_by_xpath("//*[@id='mat-tab-content-0-0']/div/div/button[1]")
 
 
-# **************************** Mashov website ****************************
-logger.info(f"Browsing to Mashov website")
+        form_element_mashov_select_school.click()
+        #form_element_mashov_select_school.send_keys(_convert(var_mashov_school_id)) # Moving to identify by school number instead of school name
+        form_element_mashov_select_school.send_keys(schoolid)
+        form_element_mashov_select_school.send_keys(Keys.ARROW_DOWN)
+        form_element_mashov_select_school.send_keys(Keys.RETURN)
 
-driver.get("https://web.mashov.info/students/login")
-#choose school:
-form_element_mashov_select_school = driver.find_element_by_xpath("//*[@id='mat-input-3']");
-#form_element_mashov_select_year = driver.find_element_by_xpath("//*[@id='mat-tab-content-0-0']/div/div/mat-form-field[2]");
-form_element_mashov_user = driver.find_element_by_xpath("//*[@id='mat-input-0']");
-form_element_mashov_password = driver.find_element_by_xpath("//*[@id='mat-input-4']");
-form_element_mashov_login = driver.find_element_by_xpath("//*[@id='mat-tab-content-0-0']/div/div/button[1]");
+        form_element_mashov_user.click()
+        form_element_mashov_user.send_keys(user)
+        form_element_mashov_password.click()
+        form_element_mashov_password.send_keys(password)
+        form_element_mashov_login.click()
+        time.sleep(3)
 
+        logger.info(f"Logged in") 
 
-form_element_mashov_select_school.click()
-#form_element_mashov_select_school.send_keys(_convert(var_mashov_school_id)) # Moving to identify by school number instead of school name
-form_element_mashov_select_school.send_keys(var_mashov_school_id)
-form_element_mashov_select_school.send_keys(Keys.ARROW_DOWN)
-form_element_mashov_select_school.send_keys(Keys.RETURN)
+        form_element_mashov_select_daily_corona_report = browser.find_element_by_xpath("//*[@id='mainView']/mat-sidenav-content/mshv-student-covidsplash/mat-card/mat-card-content/div[3]/mat-card")
+        form_element_mashov_select_daily_corona_report.click()
+        time.sleep(10)
 
+        form_element_mashov_select_option1 = browser.find_element_by_xpath("//*[@id='mat-checkbox-1']/label/div")
+        form_element_mashov_select_option2 = browser.find_element_by_xpath("//*[@id='mat-checkbox-2']/label/div")
+        form_element_mashov_check_if_selected_option1 = browser.find_element_by_xpath("//*[@id='mat-checkbox-1-input']").get_attribute("aria-checked")
+        form_element_mashov_check_if_selected_option2 = browser.find_element_by_xpath("//*[@id='mat-checkbox-2-input']").get_attribute("aria-checked")
+        form_element_mashov_submit_report = browser.find_element_by_xpath("//*[@id='mainView']/mat-sidenav-content/mshv-students-covid-clearance/mat-card/mat-card-actions/button");
 
-form_element_mashov_user.click()
-form_element_mashov_user.send_keys(var_mashov_username)
-form_element_mashov_password.click()
-form_element_mashov_password.send_keys(var_mashov_password)
-form_element_mashov_login.click()
-time.sleep(10)
-
-logger.info(f"Logged in")
-
-form_element_mashov_select_daily_corona_report = driver.find_element_by_xpath("//*[@id='mainView']/mat-sidenav-content/mshv-student-covidsplash/mat-card/mat-card-content/div[3]/mat-card");
-form_element_mashov_select_daily_corona_report.click()
-time.sleep(10)
-
-form_element_mashov_select_option1 = driver.find_element_by_xpath("//*[@id='mat-checkbox-1']/label/div");
-form_element_mashov_select_option2 = driver.find_element_by_xpath("//*[@id='mat-checkbox-2']/label/div");
-form_element_mashov_check_if_selected_option1 = driver.find_element_by_xpath("//*[@id='mat-checkbox-1-input']").get_attribute("aria-checked");
-form_element_mashov_check_if_selected_option2 = driver.find_element_by_xpath("//*[@id='mat-checkbox-2-input']").get_attribute("aria-checked");
-form_element_mashov_submit_report = driver.find_element_by_xpath("//*[@id='mainView']/mat-sidenav-content/mshv-students-covid-clearance/mat-card/mat-card-actions/button");
-
-if form_element_mashov_check_if_selected_option1 != ("true"):
-    form_element_mashov_select_option1.click()
-    time.sleep(2)
+        if form_element_mashov_check_if_selected_option1 != ("true"):
+            form_element_mashov_select_option1.click()
+            time.sleep(2)
 
 
-if form_element_mashov_check_if_selected_option2 != ("true"):
-    form_element_mashov_select_option2.click()
-    time.sleep(2)
+        if form_element_mashov_check_if_selected_option2 != ("true"):
+            form_element_mashov_select_option2.click()
+            time.sleep(2)
 
-form_element_mashov_submit_report.click()
+        form_element_mashov_submit_report.click()
 
-logger.info(f"Submitted Report")
-time.sleep(10)
+        logger.info(f"Submitted Report")
+        time.sleep(3) 
 
-driver.save_screenshot(var_screenshot_path + "/mashovkid" + var_mashov_kid_number + ".png" )
-logger.info(f"Saved Screenshot")
-logger.info("Finished Mashov Sign process for kid number: " + var_mashov_kid_number)
-logger.debug('----------------------------------------------------------------------')
+        helpers.log_browser(browser)
+        helpers.mobile_screenshot(browser,Image)        
+        
+        logger.info(f"Screenshot Saved")
+        logger.info("Finished Mashov Sign process for kid number: " + kidnum)
+        logger.debug('----------------------------------------------------------------------')        
+        
+        return 1
+    except Exception as ex:
+        logger.error(str(ex))
+        return 0
 
-driver.close()
+
+
